260547dd0a1f7a10c3e17028c452a5b9421775ea
Osifo John <osifojohntec@gmail.com>
2024-10-28 18:24:22 +0100

    feat(ui): update Orders page with filter options and info text

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -5,9 +5,20 @@ export default async function OrdersPage() {
   const orders = await api.order.getAllCustomerOrders({});
 
   return (
-    <div className="flex w-full flex-col px-4 pb-5 pt-20">
-      <h1 className="mb-4 text-3xl font-bold text-gray-800">Customer Orders</h1>
+    <main className="mx-auto max-w-7xl space-y-6 px-4 pb-14 sm:px-6 lg:px-8">
+      <div className="flex flex-col pb-8 pt-16">
+        <h1 className="mt-2 text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl lg:text-4xl">
+          Order Management
+        </h1>
+        <p className="mt-3 max-w-3xl text-base leading-relaxed text-gray-600 sm:mt-4 sm:text-lg lg:leading-loose">
+          Track and manage all customer orders in one place. Use advanced
+          filtering options to filter by order ID, customer details, or shipping
+          information. Filter by date range and order status to get precise
+          insights.
+        </p>
+      </div>
+
       <OrdersDataTable initailOrders={orders} />
-    </div>
+    </main>
   );
 }
